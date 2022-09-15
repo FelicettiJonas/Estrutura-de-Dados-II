97bbe175c73db1e454d4d9c099a1c5fcf0df5c95
Jonas Felicetti <jonasfelicetti0800@gmail.com>
2022-09-15 14:01:43 -0300

    inserção avl

--- AVL/main.c
+++ AVL/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/// @brief Estrutura do Node contendo um ponteiro pra esquerda, um pra direita e o valor contido no nodo
+/// @brief Estrutura do Node contendo um ponteiro pra esquerda, um pra direita e o valor contido no nodo e sua altura na arvore.
 typedef struct sNode
 {
     int data;
-
+    int height;
     struct sNode *left, *right;
 } Node;
 
@@ -13,6 +13,11 @@ void preOrder (Node *);
 void inOrder (Node *);
 void postOrder (Node *);
 void freeTree (Node *);
+int height(Node *);
+int max(int, int);
+Node *rightRotate(Node *);
+Node *leftRotate(Node *);
+int getBalance(Node *);
 Node *newNode(int);
 Node *insert(Node *, int);
 Node *removeRoot(Node *);
@@ -24,21 +29,21 @@ int main()
 
     root = insert(root, 5);
 
-    insert(root, 12);
-    insert(root, 2);
-    insert(root, 7);
-    insert(root, 3);
-    insert(root, 1);
-    insert(root, 10);
-    insert(root, 8);
-    insert(root, 9);
-    insert(root, 4);
-    insert(root, 6);
-    insert(root, 11);
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+    root = insert(root, 8);
+    root = insert(root, 9);
+    root = insert(root, 4);
+    root = insert(root, 6);
+    root = insert(root, 11);
 
     preOrder(root);
 
-    root = removeNode(root, 10);
+    // root = removeNode(root, 10);
 
     printf("\n");
     preOrder(root);
@@ -96,6 +101,75 @@ void freeTree (Node *node)
     }
 }
 
+/// @brief Função auxiliar para descobrir a altura de um nodo.
+/// @param node Nodo que terá a altura calculada.
+/// @return Altura do nodo.
+int height(Node *node)
+{
+    if (node == NULL)
+        return 0;
+    return 1+max(height(node->left), height(node->right));
+}
+ 
+/// @brief Função auxiliar para identificar o maior elemento dentre 2 variáveis.
+/// @param a Variável que sera comparada com b.
+/// @param b Variável que sera comparada com a.
+/// @return O maior valor dentre a e b.
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+/// @brief Algorítimo rightRotate. Realiza a troca da raiz pelo nodo a esquerda dela.
+/// @param root Raiz que será rotacionada. 
+/// @return Retorna a nova raiz.
+Node *rightRotate(Node *root)
+{
+    Node *p = root->left;
+    Node *q = p->right;
+ 
+    //Faz a rotação
+    p->right = root;
+    root->left = q;
+ 
+    //Atualiza as alturas
+    root->height = height(root);
+    p->height = height(p);
+ 
+    //Retorna a nova raiz
+    return p;
+}
+ 
+/// @brief Algorítimo leftRotate. Realiza a troca da raiz pelo nodo a direita dela.
+/// @param root Raiz que será rotacionada. 
+/// @return Retorna a nova raiz.
+Node *leftRotate(Node *root)
+{
+    Node *p = root->right;
+    Node *q = p->left;
+ 
+    //Faz a rotação
+    p->left = root;
+    root->right = q;
+ 
+    //Atualiza as alturas
+    root->height = height(root);
+    p->height = height(p);
+ 
+    //Retorna a nova raiz
+    return p;
+}
+
+/// @brief Função para descobrir o fator de balanceamento do nodo.
+/// @param node Nodo que sera balanceado.
+/// @return Fator de balanceamento do nodo.
+int getBalance(Node *node)
+{
+    if (node == NULL)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
 /// @brief Realiza o processo de alocação de memória para o novo nodo.
 /// @param data Valor contido no Node
 /// @return Ponteiro para o espaço alocado.
@@ -103,6 +177,7 @@ Node *newNode(int data)
 {
     Node *temp = (Node *)malloc(sizeof(Node));
     temp->data = data;
+    temp->height = 0;
     temp->left = temp->right = NULL;
 
     return temp;
@@ -124,6 +199,38 @@ Node *insert(Node *node, int data)
     {
         node->right = insert(node->right, data);
     }
+
+    //Atualiza a altura do nodo
+    node->height = height(node);
+ 
+   //Pega o fator de balanceamento atual
+    int balance = getBalance(node);
+ 
+    //Se o nodo estiver desbalanceado realiza o balanceamento
+ 
+    // Left Left Case
+    if (balance > 1 && data < node->left->data)
+        return rightRotate(node);
+ 
+    // Right Right Case
+    if (balance < -1 && data > node->right->data)
+        return leftRotate(node);
+ 
+    // Left Right Case
+    if (balance > 1 && data > node->left->data)
+    {
+        node->left =  leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+    // Right Left Case
+    if (balance < -1 && data < node->right->data)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+ 
+   //Retorna o nodo sem nenhuma alteração
 
     return node;
 }
